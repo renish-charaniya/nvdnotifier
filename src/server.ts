0bdf688e0c93ddaa24079775ad1e33b0f478dd3b
Renish <renish9923@gmail.com>
2024-10-08 17:09:24 +0530

    deployment test

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import { setLastScannedDate } from "./utils/helpers";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on the port ${PORT}`);
   setLastScannedDate();
 });
