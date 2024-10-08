bb837c171f4f8528bef4f6c50e10ae86ac8cbe10
Renish <renish9923@gmail.com>
2024-10-09 02:01:45 +0530

    fix(cronSettings): Added support for dbcheck endpoint vercel cron every day 1:45 AM

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,6 +9,6 @@ export const config = {
   scanInterval: parseInt(<string>process.env.SCAN_INTERVAL) || 10,
   nvdApiUrl: "https://services.nvd.nist.gov/rest/json/cves/2.0",
   mongodbUri:
-    process.env.MONGODB_URI || "mongodb://localhost:27017/nvdnotifier",
+    process.env.DATABASE_URL || "mongodb://localhost:27017/nvdnotifier",
   dbName: process.env.DB_NAME,
 };
